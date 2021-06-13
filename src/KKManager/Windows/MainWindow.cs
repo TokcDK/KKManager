4f3d4405b4d5de5f681d2d1d245b869f29e286b9
Denis K <tokcdk@gmail.com>
2021-06-13 17:57:06 +0500

    moved 2 sb3ugs checks to sb3ugs

--- src/KKManager/Windows/MainWindow.cs
+++ src/KKManager/Windows/MainWindow.cs
@@ -592,71 +592,11 @@ namespace KKManager.Windows
 
 
                 var files = directories.SelectMany(dir => dir.GetFiles("*", SearchOption.AllDirectories))
-                    .Where(file => FileCanBeCompressed(file, rootDirectory))
+                    .Where(file => SB3UGS_Checks.FileCanBeCompressed(file, rootDirectory))
                     .ToList();
 
-                CompressFiles(files, false);
+                SB3UGS_Checks.CompressFiles(files, false);
             }
-        }
-
-        private bool FileCanBeCompressed(FileInfo x, DirectoryInfo rootDirectory)
-        {
-            if (!SB3UGS_Utils.FileIsAssetBundle(x)) return false;
-
-            // Files inside StreamingAssets are hash-checked so they can't be changed
-            var isStreamingAsset = x.FullName.Substring(rootDirectory.FullName.Length)
-                .Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)
-                .Contains("StreamingAssets", StringComparer.OrdinalIgnoreCase);
-            if (isStreamingAsset)
-            {
-                Console.WriteLine($"Skipping file {x.FullName} - Files inside StreamingAssets folder are hash-checked and can't be modified.");
-                return false;
-            }
-
-            return true;
-        }
-
-        private void CompressFiles(IReadOnlyList<FileInfo> files, bool randomizeCab)
-        {
-            if (!SB3UGS_Initializer.CheckIsAvailable())
-            {
-                MessageBox.Show(
-                    T._("SB3UGS has not been found in KK Manager directory or it failed to be loaded. Reinstall KK Manager and try again."),
-                    T._("Compress files"), MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            LoadingDialog.ShowDialog(this, T._("Compressing asset bundle files"), dialogInterface =>
-            {
-                dialogInterface.SetMaximum(files.Count);
-
-                var excs = new ConcurrentBag<Exception>();
-                long totalSizeSaved = 0;
-                var count = 0;
-
-                Parallel.ForEach(files, file =>
-                {
-                    dialogInterface.SetProgress(count++, T._("Compressing ") + file.Name);
-
-                    try
-                    {
-                        var origSize = file.Length;
-                        SB3UGS_Utils.CompressBundle(file.FullName, randomizeCab);
-                        file.Refresh();
-                        totalSizeSaved += origSize - file.Length;
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(T._($"Failed to compress file {file.FullName} - {ex.Message}"));
-                        excs.Add(ex);
-                    }
-                });
-
-                if (excs.Any())
-                    MessageBox.Show(T._($"Successfully compressed {files.Count - excs.Count} out of {files.Count} files, see log for details. Saved {FileSize.FromBytes(totalSizeSaved).ToString()}."), T._("Compress files"), MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                else
-                    MessageBox.Show(T._($"Successfully compressed {files.Count} files. Saved {FileSize.FromBytes(totalSizeSaved).ToString()}."), T._("Compress files"), MessageBoxButtons.OK, MessageBoxIcon.Information);
-            });
         }
 
         private void compressBundlesAndRandomizeCABsToolStripMenuItem_Click(object sender, EventArgs e)
@@ -672,14 +612,14 @@ namespace KKManager.Windows
                 {
                     var rootDirectory = new DirectoryInfo(d.FileName);
                     var files = rootDirectory.GetFiles("*", SearchOption.AllDirectories)
-                        .Where(file => FileCanBeCompressed(file, rootDirectory))
+                        .Where(file => SB3UGS_Checks.FileCanBeCompressed(file, rootDirectory))
                         .ToList();
 
                     var randomize = MessageBox.Show("Do you want to randomize CABs of the compressed files? Click No to keep the original CAB strings.",
                                         "Compress bundles in a folder...", MessageBoxButtons.YesNo, MessageBoxIcon.Question) ==
                                     DialogResult.Yes;
 
-                    CompressFiles(files, randomize);
+                    SB3UGS_Checks.CompressFiles(files, randomize);
                 }
             }
         }
